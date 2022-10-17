7c2e66027b80b29696ba17bab5295ea265b723e7
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-10-17 18:40:13 +0500

    feat: add solutions count-cats and sum-digits

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function countCats(matrix) {
+  let newMatrix = matrix.flat();
+
+  function cat(value) {
+    return value === "^^";
+  }
+
+  let result = newMatrix.filter(cat);
+
+  if (result.length < 1 || result.length === undefined) {
+    return 0;
+  } else {
+    return result.length;
+  }
 }
 
+countCats([
+  [0, 1, "^^"],
+  [0, "^^", 2],
+  ["^^", 1, 2],
+]);
+
 module.exports = {
-  countCats
+  countCats,
 };
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Given a number, replace this number with
@@ -12,9 +14,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits( /* n */ ) {
+  function getSumOfDigits(n) {
+    strN = String(n);
+    arrN = strN.split('');
+    let sum = 0;
+
+    for (let i = 0; arrN.length > i; i++) {
+      sum += (+arrN[i]);
+      return (sum);
+    }
+    if (sum > 10 ) {
+      sum = Math.floor(sum / 10) + sum % 10
+    }
+    return (sum);
+  }
+
+  getSumOfDigits(395)
 }
 
 module.exports = {
