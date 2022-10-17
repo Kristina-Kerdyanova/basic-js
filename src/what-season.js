3e00f4b59010b95f3a2d344d0671ecab289778ab
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-10-17 23:57:05 +0500

    fix: fix solution what season

--- src/what-season.js
+++ src/what-season.js
@@ -15,22 +15,25 @@ const {
  */
 
 function getSeason(date) {
-    if ((date.getMonth() > 10) || (date.getMonth() < 2)) {
+  if (date == undefined) return 'Unable to determine the time of year!';
+
+  if (Object.prototype.toString.call(date) !== '[object Date]') throw new Error('Invalid date!');
+
+  if (date.hasOwnProperty('toString')) throw new Error('Invalid date!');
+
+  if ((date.getMonth() > 10) || (date.getMonth() < 2)) {
     return ('winter');
-  } else if ((date.getMonth() > 1) && (date.getMonth() < 5)) {
+  } 
+  if ((date.getMonth() > 1) && (date.getMonth() < 5)) {
     return ('spring');
-  } else if ((date.getMonth() > 4) && (date.getMonth() <8)) {
+  } 
+  if ((date.getMonth() > 4) && (date.getMonth() < 8)) {
     return ('summer');
-  } else if ((date.getMonth() > 7) && (date.getMonth() < 11)) {
+  }
+  if ((date.getMonth() > 7) && (date.getMonth() < 11)) {
     return ('autumn');
-  } else if (date.getMonth() === undefined) {
-    return 'Unable to determine the time of year!'; 
-  } else {
-    throw new Error ('Invalid date!')
   }
 }
-
-getSeason(new Date(2019, 8, 22, 3, 0, 11, 500));
 
 module.exports = {
   getSeason
